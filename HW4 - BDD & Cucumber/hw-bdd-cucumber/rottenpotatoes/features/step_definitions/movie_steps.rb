865a827b42a5e7f91aa655c4761e2ecfe4ad06ff
FateXRebirth <FateXRebirth@gmail.com>
2017-07-16 20:55:14 +0800

    Homework 4 done

---
+++ HW4 - BDD & Cucumber/hw-bdd-cucumber/rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -0,0 +1,7 @@
+When(/^I go to the edit page for "([^"]*)"$/) do |arg1|
+  puts arg1
+end
+
+Then(/^the director of "([^"]*)" should be "([^"]*)"$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
